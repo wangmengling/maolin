8f21065a800f2301ffe04a3e31c631ac21b591c6
wangmengling <294025529@qq.com>
2016-04-05 17:18:37 +0800

    react react-redux

--- client/main.js
+++ client/main.js
@@ -1,22 +1,20 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import { Meteor } from 'meteor/meteor';
+import 'babel-polyfill'
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import App from '../project/containers/App'
+import configureStore from '../project/store/configureStore'
+import 'todomvc-app-css/index.css'
 
-import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+Meteor.startup(() => {
+  const store = configureStore()
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+  )
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,9 @@
-
 var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
-  entry: [
-    'webpack-hot-middleware/client',
-    './index'
-  ],
+  // devtool: 'cheap-module-eval-source-map',
+  entry: "./client/main.js",
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
@@ -23,6 +19,11 @@ module.exports = {
         test: /\.js$/,
         loaders: [ 'babel' ],
         exclude: /node_modules/,
+        include: __dirname
+      },
+      {
+        test: /\.css?$/,
+        loaders: [ 'style', 'raw' ],
         include: __dirname
       }
     ]
